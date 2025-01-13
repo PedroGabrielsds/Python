283ed0c14cf1bc19f7142fb17f0b2c65f00744ef
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-01-13 15:55:37 -0300

    Ex004

--- CursoPython/Aula 06 - Python/Ex004.py
+++ CursoPython/Aula 06 - Python/Ex004.py
@@ -1,1 +1,13 @@
 #Desafio 004 - Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todos as informações possiveis sobre ele
+
+algo = input("Digite algo: ")
+
+print("O tipo primitivo desse valor é: ", type(algo))
+print(f"É Alphanumerico: ",algo.isalnum())
+print(f"São letras: ", algo.isalpha())
+print(f"São números: ", algo.isnumeric())
+print(f"É maiúsculo: ", algo.isupper())
+print(f"É minúsculo: ", algo.islower())
+print(f"É um titulo? ", algo.istitle())
+print(f"Tem decimal? ", algo.isdecimal())
+print(f"É espaço em branco? ", algo.isspace())
