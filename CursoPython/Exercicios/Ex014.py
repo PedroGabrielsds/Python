d86ceb5d02cf7afef0ad93f2784fd5d97e6b7b7f
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-01-21 15:03:56 -0300

    Ex014 - Python

---
+++ CursoPython/Exercicios/Ex014.py
@@ -0,0 +1,10 @@
+#Faça um programa que converta uma temperatura em graus Celsius para Fahrenheit:
+
+graus = float(input("Informe a temperatura em ºC: "))
+
+fahrenheit = float((graus * 9/5) + 32)
+
+print(f"A temperatura de {graus}ºC corresponde a {fahrenheit}ºF!")
+
+
+
