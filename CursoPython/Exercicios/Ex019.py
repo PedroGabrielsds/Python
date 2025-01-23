87cb2844f12a7784768346acc64aa9c2de8e47c0
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-01-23 15:56:03 -0300

    Exercicios aula 08 - Python

--- CursoPython/Exercicios/Ex019.py
+++ CursoPython/Exercicios/Ex019.py
@@ -1,3 +1,16 @@
 #Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
 # Faça um programa que ajude ele, lendo o nome deels e escrevendo o nome escolhido.
 
+import random
+
+aluno_1 = input("1º aluno(a): ")
+aluno_2 = input("2º aluno(a): ")
+aluno_3 = input("3º aluno(a): ")
+aluno_4 = input("4º aluno(a): ")
+
+alunos = [aluno_1, aluno_2, aluno_3, aluno_4]
+
+aluno_escolhido = random.choice(alunos)
+
+print(f"O aluno(a) escolhido foi: {aluno_escolhido}")
+
--- CursoPython/Exercicios/Ex020.py
+++ CursoPython/Exercicios/Ex020.py
@@ -1,2 +1,15 @@
 #O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos.
 #Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+import random
+
+import random
+
+aluno_1 = input("1º aluno: ")
+aluno_2 = input("2º aluno: ")
+aluno_3 = input("3º aluno: ")
+aluno_4 = input("4º aluno: ")
+
+alunos = [aluno_1, aluno_2, aluno_3, aluno_4]
+random.shuffle(alunos)
+print(alunos)
+
