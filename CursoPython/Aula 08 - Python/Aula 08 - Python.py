3a32582992e8ebd43dcf25a7a77e9ddcd35fdfa3
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-03-17 15:17:22 -0300

    Organizando Repositório

--- CursoPython/Aula 08 - Python/Aula 08 - Python.py
+++ CursoPython/Aula 08 - Python/Aula 08 - Python.py
@@ -1,12 +1,12 @@
 #Utilizando módulos:
 
 import math
-num = int(input("Digite um número: "))
-raiz = math.sqrt(num)
-print(f"a raiz de {num} é {raiz}")
+#num = int(input("Digite um número: "))
+#raiz = math.sqrt(num)
+#print(f"a raiz de {num} é {raiz}")
 
 import emoji
-print(emoji.emojize("Olá :smiling_face_with_sunglasses:", language='en'))
+print(emoji.emojize("Olá :globe_showing_Americas:", language='en'))
 
 
 
