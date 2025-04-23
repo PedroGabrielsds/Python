5d719ca68475fb185917600c1a15b3fd3c3e7d3b
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-04-23 17:48:45 -0300

    Ex041 - Python

--- CursoPython/Mundo 2 - Python/Exercícios/Ex041.py
+++ CursoPython/Mundo 2 - Python/Exercícios/Ex041.py
@@ -11,3 +11,25 @@
 #- Até 20 anos: Sênior
 #- Acima: Master
 
+
+print("\033[1;36;40m-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-\033[m")
+print("\033[1;36;40m|\033[m  \033[1;32;40mSeja bem vindo à Confederação \033[m\033[1;33;40mNacional de Natação\033[m  \033[1;36;40m|\033[m")
+print("\033[1;36;40m-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-\033[m")
+
+idade = int(input("Digite sua idade e descubra sua categoria de nado: "))
+
+if(idade <= 9):
+    print(f"Idades abaixo de {idade} é da categoria: Mirim")
+    print(f"Sua categoria de nado é: MIRIM")
+elif(idade <= 14):
+    print(f"Idades abaixo de {idade} são da categoria: Infantil")
+    print(f"Sua categoria de nado é: Infantil")
+elif(idade <= 19):
+    print(f"Idades abaixo de {idade} são da categoria: Junior")
+    print(f"Sua categoria de nado é: Junior")
+elif(idade <= 20):
+    print(f"Idades abaixo de {idade} são da categoria: Sênior")
+    print(f"Sua categoria de nado é: Sênior")
+else:
+    print(f"Idades acima de {idade} são da categoria: Master")
+    print(f"Sua categoria de nado é: Master")
