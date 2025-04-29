02123d5e69ab61f0a62d9cc9cb9bae88d987d83b
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-04-29 18:37:07 -0300

    Update Ex045 - Pyhton Mundo 2

--- CursoPython/Mundo 2 - Python/Exercícios/Ex045.py
+++ CursoPython/Mundo 2 - Python/Exercícios/Ex045.py
@@ -6,16 +6,63 @@
 
 import random
 
-jogadas = ("Pedra", "Papel", "Tesoura")
+print("\033[1;31;40m=--\033[m" * 5,"JOKENPÔ", "\033[1;31;40m--=\033[m" * 5)
 
-computador = random.choice(jogadas)
+jogador = str(input("Qual seu nickname? "))
 
-print("\033[1;31;40m=--\033[m" * 5,"\033[1;37;40mPedra\033[m, Papel, Tesoura!", "\033[1;31;40m--=\033[m" * 5)
+opcao = int(input("""\033[1;37;40m[1] - Pedra\033[m 
+\033[1;38;40m[2] - Papel\033[m
+\033[1;31;40m[3] - Tesoura\033[m
+\033[1;33;40mEscolha sua jogada:\033[m """))
 
-opcao = str(input("""[1] - Pedra 
-[2] - Papel
-[3] - Tesoura
-Escolha sua jogada: """))
+if((opcao != 1) and (opcao != 2) and (opcao != 3)):
+    print("Opção inválida tente novamente...")
+else:
+    print("Jo")
+    print("Ken")
+    print("Pô!")
+
+    jogadas = ("Pedra", "Papel", "Tesoura")
+    computador = random.choice(jogadas)
+
+    if():
+        print(f"Computador: {computador}")
+        print(f"{jogador}: Papel")
+        print(f"Empate!! Tente novamente...")
+    else:
+        if(() and ()):
+            print(f"Computador: {computador}")
+            print(f"{jogador}: Tesoura")
+            print("O computador ganhou, tente mais uma vez!")
+
+        elif(() and ()):
+            print(f"Computador: {computador}")
+            print(f"{jogador}: Papel")
+            print("O computador ganhou, tente mais uma vez!")
+
+        elif(() and ()):
+            print(f"Computador: {computador}")
+            print(f"{jogador}: Pedra")
+            print(f"O computador ganhou, tente mais uma vez!")
+
+        elif(() and ()):
+            print(f"Computador: {computador}")
+            print(f"{jogador}: Tesoura")
+            print(f"Winner!!")
+            print("Vencedor!")
+
+        elif(() and ()):
+            print(f"Computador: {computador}")
+            print(f"{jogador}: Papel")
+            print(f"Winner!!")
+            print("Vencedor!")
+
+        elif(() and ()):
+            print(f"Computador: {computador}")
+            print(f"{jogador}: Pedra")
+            print(f"Winner!!")
+            print("Vencedor!")
+
 
 
 
