bc5f6364cc2059b2878a35d9c9b109a0bc7229ae
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-05-07 19:02:03 -0300

    Ex045 - Mundo 2 Python

--- CursoPython/Mundo 2 - Python/Exercícios/Ex045.py
+++ CursoPython/Mundo 2 - Python/Exercícios/Ex045.py
@@ -8,83 +8,38 @@ import random
 
 print("\033[1;31;40m=--\033[m" * 5,"JOKENPÔ", "\033[1;31;40m--=\033[m" * 5)
 
-jogadas = [1, 2, 3]
-maquina = random.choice(jogadas)
-print(maquina)
-if (maquina == 1):
-    computador = "Pedra"
-elif (maquina == 2):
-    computador = "Papel"
-elif (maquina == 3):
-    computador = "Tesoura"
-else:
-    print("\033[1;31;40mError!!\033[m")
+jogadas = ('Pedra', 'Papel', 'Tesoura')
+maquina = random.randint(0, 2)
 
 jogador = str(input("\033[1;33;40mQual seu nickname? \033[m"))
 
-escolha = int(input("""\033[1;37;40m[1] - Pedra\033[m 
-\033[1;38;40m[2] - Papel\033[m
-\033[1;31;40m[3] - Tesoura\033[m
+escolha = int(input("""\033[1;37;40m[0] - Pedra\033[m 
+\033[1;38;40m[1] - Papel\033[m
+\033[1;31;40m[2] - Tesoura\033[m
 \033[1;33;40mEscolha sua jogada:\033[m """))
 
-if(escolha == 1):
-    escolha_jogador = "Pedra"
-elif(escolha == 2):
-    escolha_jogador = "Papel"
-elif(escolha == 3):
-    escolha_jogador = "Tesoura"
-else:
-    print("\033[1;31;40mOpção inválida tente novamente...\033[m")
-
 if(maquina == escolha):
-    print(f"Máquina: {computador}")
-    print(f"{jogador}: {escolha_jogador}")
+
+    print(f"Máquina: {jogadas[maquina]}")
+    print(f"{jogador}: {jogadas[escolha]}")
     print(f"Empate!! Tente novamente...")
+
 else:
+
     print("\033[1;37;40mJo\033[m")
     print("\033[1;38;40mKen\033[m")
     print("\033[1;31;40mPô!\033[m")
 
-    if((escolha == 1) and (maquina == 2)):
-        print(f"Máquina: {computador}")
-        print(f"{jogador}: {escolha_jogador}")
-        print(f"\033[mA Máquina venceu!!\033[m")
-    elif((escolha == 2) and (maquina == 3)):
-        print(f"Máquina: {computador}")
-        print(f"{jogador}: {escolha_jogador}")
-        print(f"A Máquina venceu!!")
-    elif((escolha == 3) and (maquina == 1)):
-        print(f"Máquina: {computador}")
-        print(f"{jogador}: {escolha_jogador}")
-        print(f"A Máquina venceu!!")
-    elif ((maquina == 1) and (escolha == 2)):
-        print(f"Máquina: {computador}")
-        print(f"{jogador}: {escolha_jogador}")
-        print(f"\033[mVocê venceu!!\033[m")
+    if(escolha == 0 and maquina == 2) or (escolha == 1 and maquina == 0) or (escolha == 2 and maquina == 1):
+
+        print(f"Máquina: {jogadas[maquina]}")
+        print(f"{jogador}: {jogadas[escolha]}")
         print(f"Winner!!")
-    elif ((maquina == 2) and (escolha == 3)):
-        print(f"Máquina: {computador}")
-        print(f"{jogador}: {escolha_jogador}")
-        print(f"\033[mVocê venceu!!\033[m")
-        print(f"Winner!!")
-    elif ((maquina == 3) and (escolha == 1)):
-        print(f"Máquina: {computador}")
-        print(f"{jogador}: {escolha_jogador}")
-        print(f"\033[mVocê venceu!!\033[m")
-        print(f"Winner!!")
+        print(f"Você ganhou, parabéns!!")
 
+    else:
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        print(f"Máquina: {jogadas[maquina]}")
+        print(f"{jogador}: {jogadas[escolha]}")
+        print(f"\033[1;31;40mLoser!!\033[m")
+        print(f"Você perdeu!! Tente novamente...")
