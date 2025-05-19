d76da20e17f36573500b98ade7e6eeb49259f5b0
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-05-19 17:51:48 -0300

    Ex050 - Mundo 2 Python

--- CursoPython/Mundo 2 - Python/Exercícios/Ex049.py
+++ CursoPython/Mundo 2 - Python/Exercícios/Ex049.py
@@ -1,3 +1,13 @@
 #Ex049 - Aula 13
 
 #Refaça o desafio 09, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for
+
+print("\033[1;33;40m-=\033[m" * 14)
+print("\033[1;33;40m-=\033[m" * 5, "\033[1;32;40mTabuada\033[m", "\033[1;33;40m-=\033[m" * 4)
+print("\033[1;33;40m-=\033[m" * 14)
+
+tabuada_numero = int(input("Qual tabuada deseja ver? "))
+
+for contador in range(1, 11):
+    resultado = (tabuada_numero * contador)
+    print(f"{tabuada_numero} X {contador} = {resultado}")
--- CursoPython/Mundo 2 - Python/Exercícios/Ex050.py
+++ CursoPython/Mundo 2 - Python/Exercícios/Ex050.py
@@ -2,4 +2,11 @@
 
 #Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares.
 #Se o valor digitado for ímpar, desconsidere-o
+soma_pares = 0
 
+for contador in range(1, 7):
+    numero = int(input(f"Digite o {contador}º número: "))
+    if (numero % 2 == 0):
+        soma_pares += numero
+
+print(f"A soma de todos os números pares resultou em: {soma_pares}")
