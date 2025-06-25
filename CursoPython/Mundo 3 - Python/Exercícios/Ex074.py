85e40cfd9b4d313738d73b65c31427bb0a8d9049
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-06-25 17:12:38 -0300

    Exercícios Aula 16 - Python Mundo 2

--- CursoPython/Mundo 3 - Python/Exercícios/Ex074.py
+++ CursoPython/Mundo 3 - Python/Exercícios/Ex074.py
@@ -2,16 +2,3 @@
 
 #Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla.
 #Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
-
-from random import randint
-
-numeros_sorteados = (0, )
-contador = 1
-while(contador < 5):
-    numero_sorteado = randint(1, 10)
-    numeros_sorteados += numero_sorteado + ','
-    contador += 1
-
-
-
-print(numeros_sorteados)
