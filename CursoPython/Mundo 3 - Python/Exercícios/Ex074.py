c7788b694fba681ddc61b4a62cb20fd4ff63a660
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-06-26 14:23:39 -0300

    Ex074 - Python Mundo 3

--- CursoPython/Mundo 3 - Python/Exercícios/Ex074.py
+++ CursoPython/Mundo 3 - Python/Exercícios/Ex074.py
@@ -2,3 +2,25 @@
 
 #Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla.
 #Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
+
+from random import randint
+maior = 0
+menor = 0
+contador = 0
+
+numeros_aleatorios = (randint(1,10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+
+print(f"Números gerados foram:", numeros_aleatorios)
+for numero in numeros_aleatorios:
+    contador += 1
+    if(contador == 1):
+        menor = numero
+        maior = numero
+    else:
+        if(numero > maior):
+            maior = numero
+        elif(numero < menor):
+            menor = numero
+
+print(f"O maior número gerado é: {maior}")
+print(f"O menor número gerado é: {menor}")
