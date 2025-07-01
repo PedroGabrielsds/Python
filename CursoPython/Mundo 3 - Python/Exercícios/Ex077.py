7909ed5a0bb2889019c73354befc6a2845df9fb2
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-07-01 18:24:04 -0300

    Ex077 - Python Mundo 2

--- CursoPython/Mundo 3 - Python/Exercícios/Ex077.py
+++ CursoPython/Mundo 3 - Python/Exercícios/Ex077.py
@@ -3,3 +3,11 @@
 #Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada
 #palavra, quais são as suas vogais.
 
+tupla_palavras = ('Ponche', 'Amazon', 'Fiat', 'Chevrolet', 'Ford', 'Porsche', 'Mercedes', 'Cadeira', 'Carro', 'Pizza',
+                  'Hamburguer', 'Salgado', 'Doce', 'Parede', 'Musica', 'Jogo')
+
+
+for palavra in tupla_palavras:
+    palavra_dividida = palavra.lower().split()
+    print(palavra_dividida[0] [palavra_dividida.count('a')])
+    #print(f"A palavra: {palavra} contém {contagem_vogais}, sendo elas: {}")
