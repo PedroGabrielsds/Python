397dfc712ddc06fb9c80bf12568d9e2ca592c2cb
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-07-01 18:23:48 -0300

    Ex076 - Python Mundo 2

--- CursoPython/Mundo 3 - Python/Exercícios/Ex076.py
+++ CursoPython/Mundo 3 - Python/Exercícios/Ex076.py
@@ -3,3 +3,11 @@
 #Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência.
 #No final, mostre uma listagem de preços. organizando os dados em forma tabular.
 
+contador = 0
+tupla_produtos = ("Lápis", 1.75, "Borracha", 2.00, "Caderno", 15.90, "Estojo", 25.00, "Transferidor", 4.20, "Compasso", 9.99, "Mochila", 120.32,
+                  "Canetas", 22.30, "Livro", 34.90)
+
+print("-" * 30)
+print(f"{'Produtos':<25}", end="Preço\n")
+print("-" * 30)
+
