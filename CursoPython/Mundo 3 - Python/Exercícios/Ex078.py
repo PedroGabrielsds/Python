c2843f9540a36cfafef96f7e28e73e252c387304
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-07-08 17:37:46 -0300

    Ex078 - Python Mundo 3

--- CursoPython/Mundo 3 - Python/Exercícios/Ex078.py
+++ CursoPython/Mundo 3 - Python/Exercícios/Ex078.py
@@ -5,13 +5,24 @@
 #No final, mostre qual foi o maior e menor valor digitado e as suas respectivas posições na lista.
 
 contador = 1
+maior_valor = 0
+menor_valor = 0
 numeros = list()
+
 while(len(numeros) < 5):
     numeros.append(int(input(f"Digite o {contador}º valor: ")))
     contador += 1
 
 for posicao, valor in enumerate(numeros):
     if(valor == max(numeros)):
-        print(f"O maior valor digitado foi {max(numeros)} localizado na posição: {posicao + 1}")
-    elif(valor == min(numeros)):
-        print(f"O menor valor digitado foi {min(numeros)} localizado na posição: {posicao + 1}")
+        if(maior_valor >= 1):
+            print(end=f", {posicao}")
+        else:
+            print(f"\nO maior valor digitado foi {max(numeros)} localizado na posição: {posicao + 1}", end=" ")
+        maior_valor += 1
+    if(valor == min(numeros)):
+        if(menor_valor >= 1):
+            print(end=f", {posicao}")
+        else:
+            print(f"\nO menor valor digitado foi {min(numeros)} localizado na posição: {posicao + 1}", end=" ")
+        menor_valor += 1
