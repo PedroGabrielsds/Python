b3866e68f0945a912d972c69a84e6288a12c22a1
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-07-14 17:44:05 -0300

    Ex078 - Python Mundo 3

--- CursoPython/Mundo 3 - Python/Exercícios/Ex078.py
+++ CursoPython/Mundo 3 - Python/Exercícios/Ex078.py
@@ -7,6 +7,7 @@
 contador = 1
 maior_valor = 0
 menor_valor = 0
+plural = False
 numeros = list()
 
 while(len(numeros) < 5):
@@ -15,7 +16,34 @@ while(len(numeros) < 5):
 
 print(f"=-" * 30)
 
-for contador in range(0, len(numeros)):
-    for posicao, valor in enumerate(numeros):
-        if(valor == max(numeros)):
-            print(f"O MAIOR valor digitado foi {max(numeros)} localizado na posição: ", end="")
+contador = 0
+for posicao, valor in enumerate(numeros):
+    if(valor == max(numeros)):
+        contador += 1
+        if(contador > 1):
+            plural = True
+        if(valor == max(numeros) and contador == 1):
+            if(plural == True):
+                print(f"O MAIOR valor digitado foi {max(numeros)} localizado nas posições: {posicao + 1}", end="")
+            else:
+                print(f"O MAIOR valor digitado foi {max(numeros)} localizado na posição: {posicao + 1}", end="")
+        elif(valor == max(numeros) and (contador > 1)):
+            print(f", {posicao + 1}", end="")
+
+print("")
+plural = False
+contador = 0
+for posicao, valor in enumerate(numeros):
+    if(valor == min(numeros)):
+        contador += 1
+        if(contador > 1):
+            plural = True
+        if((valor == min(numeros)) and (contador == 1)):
+            if(plural == True):
+                print(f"O MENOR valor digitado foi {min(numeros)} localizado nas posições: {posicao + 1}", end="")
+            else:
+                print(f"O MENOR valor digitado foi {min(numeros)} localizado na posição: {posicao + 1}", end="")
+        elif(valor == min(numeros) and contador > 1):
+            print(f", {posicao + 1}")
+
+#Análisar como fazer funcionar o plural e como diminuir código de verificação para maior e menor - Nota 14/07/2025
