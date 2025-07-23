787b7342e798ae470af4c2fc45ed4ce2ee27559f
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-07-23 17:19:15 -0300

    Ex080 - Python Mundo 3

--- CursoPython/Mundo 3 - Python/Exercícios/Ex080.py
+++ CursoPython/Mundo 3 - Python/Exercícios/Ex080.py
@@ -4,3 +4,30 @@
 #correta de inserção (sem o usuário usar o sort()).
 
 #No final, mostre a lista ordenada na tela.
+
+numeros = list()
+
+while(len(numeros) < 5):
+    if(len(numeros) == 0):
+        valor = int(input(f"Digite um número: "))
+        numeros.append(valor)
+        print(f"Adicionado ao final da lista...")
+    else:
+        valor = int(input(f"Digite um número: "))
+        for posicao, numero in enumerate(numeros):
+            if(valor == numero):
+                numeros.insert(posicao, valor)
+                print(f"Adicionado na posição {posicao} da lista...")
+                break
+            elif(valor < numero):
+                 numeros.insert(posicao, valor)
+                 print(f"Adicionado na posição {posicao} da lista...")
+                 break
+            elif(valor > numero):
+                if(posicao + 1 == len(numeros) and valor > numero):
+                    numeros.append(valor)
+                    print(f"Adicionado ao final da lista...")
+                    break
+
+print(f"-=-=" * 20)
+print(f"Os valores digitados em ordem foram {numeros}")
