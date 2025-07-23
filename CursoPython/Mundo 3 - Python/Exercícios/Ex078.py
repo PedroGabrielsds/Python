82b85e32fa109839c767c46d3907696640e6ea06
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-07-23 17:18:38 -0300

    Ex078 - Python Mundo 3

--- CursoPython/Mundo 3 - Python/Exercícios/Ex078.py
+++ CursoPython/Mundo 3 - Python/Exercícios/Ex078.py
@@ -5,11 +5,8 @@
 #No final, mostre qual foi o maior e menor valor digitado e as suas respectivas posições na lista.
 
 contador = 1
-contador_max = 0
-contador_min = 0
-maior_valor = 0
-menor_valor = 0
-plural = False
+singular = "na posição"
+plural = "nas posições"
 numeros = list()
 posicoes_max = list()
 posicoes_min = list()
@@ -27,35 +24,41 @@ for posicao, valor in enumerate(numeros):
         posicoes_min.append(posicao + 1)
 
 print(f"Você digitou os valores {numeros}")
-print(f"O maior valor digitado foi: {max(numeros)} nas posições {posicoes_max}")
-print(f"O menor valor digitado foi: {min(numeros)} nas posições {','.join(posicoes_min)}")
+if(len(posicoes_max) > 1):
+    print(f"O maior valor digitado foi: \033[1;37m{max(numeros)}\033[m {plural} {", ".join(map(str, posicoes_max))}")
+elif(len(posicoes_max) == 1):
+    print(f"O maior valor digitado foi: \033[1;37m{max(numeros)}\033[m {singular} {", ".join(map(str, posicoes_max))}")
 
-for posicao, valor in enumerate(numeros):
-    if(posicao == 0 and valor == max(numeros)):
-        if(valor == max(numeros)):
-            contador_max += 1
-            if(contador_max > 1):
-                plural = True
-            if(valor == max(numeros) and contador_max == 1):
-                if(plural == True):
-                    print(f"O MAIOR valor digitado foi {max(numeros)} localizado nas posições: {posicao + 1}", end="")
-                else:
-                    print(f"O MAIOR valor digitado foi {max(numeros)} localizado na posição: {posicao + 1}", end="")
-            elif(valor == max(numeros) and (contador_max > 1)):
-                print(f", \033[1;32m{posicao + 1}", end="")
-        elif(valor == min(numeros)):
-            plural = False
-            for posicao, valor in enumerate(numeros):
-                if(valor == min(numeros)):
-                    contador_min += 1
-                    if(contador_min > 1):
-                        plural = True
-                    if((valor == min(numeros)) and (contador_min == 1)):
-                        if(plural == True):
-                            print(f"O MENOR valor digitado foi {min(numeros)} localizado nas posições: {posicao + 1}", end="")
-                        else:
-                            print(f"O MENOR valor digitado foi {min(numeros)} localizado na posição: {posicao + 1}", end="")
-                    elif(valor == min(numeros) and contador_min > 1):
-                        print(f", \033[1;31m{posicao + 1}")
+if(len(posicoes_min) > 1):
+    print(f"O menor valor digitado foi: \033[1;37m{min(numeros)}\033[m {plural} {", ".join(map(str, posicoes_min))}")
+elif(len(posicoes_min) == 1):
+    print(f"O menor valor digitado foi: \033[1;37m{min(numeros)}\033[m {singular} {", ".join(map(str, posicoes_min))}")
 
-#Análisar como fazer funcionar o plural e como diminuir código de verificação para maior e menor - Nota 14/07/2025
+#Primeira tentativa:
+# for posicao, valor in enumerate(numeros):
+#     if(posicao == 0 and valor == max(numeros)):
+#         if(valor == max(numeros)):
+#             contador_max += 1
+#             if(contador_max > 1):
+#                 plural = True
+#             if(valor == max(numeros) and contador_max == 1):
+#                 if(plural == True):
+#                     print(f"O MAIOR valor digitado foi {max(numeros)} localizado nas posições: {posicao + 1}", end="")
+#                 else:
+#                     print(f"O MAIOR valor digitado foi {max(numeros)} localizado na posição: {posicao + 1}", end="")
+#             elif(valor == max(numeros) and (contador_max > 1)):
+#                 print(f", \033[1;32m{posicao + 1}", end="")
+#         elif(valor == min(numeros)):
+#             plural = False
+#             for posicao, valor in enumerate(numeros):
+#                 if(valor == min(numeros)):
+#                     contador_min += 1
+#                     if(contador_min > 1):
+#                         plural = True
+#                     if((valor == min(numeros)) and (contador_min == 1)):
+#                         if(plural == True):
+#                             print(f"O MENOR valor digitado foi {min(numeros)} localizado nas posições: {posicao + 1}", end="")
+#                         else:
+#                             print(f"O MENOR valor digitado foi {min(numeros)} localizado na posição: {posicao + 1}", end="")
+#                     elif(valor == min(numeros) and contador_min > 1):
+#                         print(f", \033[1;31m{posicao + 1}")
