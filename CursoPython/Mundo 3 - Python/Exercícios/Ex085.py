c4c30f705e350861fcbd2f0dba54d788921425b6
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-08-07 17:53:36 -0300

    Ex085 - Python Mundo 3

--- CursoPython/Mundo 3 - Python/Exercícios/Ex085.py
+++ CursoPython/Mundo 3 - Python/Exercícios/Ex085.py
@@ -2,3 +2,25 @@
 
 #Crie um programa no qual o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que
 #matenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
+
+valores = []
+pares = []
+impares = []
+
+for contador in range(1, 8):
+    valores.append(int(input(f"Digite o {contador}º valor: ")))
+    if(valores[0] % 2 == 0):
+        pares.append(valores[:])
+    else:
+        impares.append(valores[:])
+
+print(valores[0])
+
+# valores.append(pares[:])
+# valores.append(impares[:])
+# print(pares)
+# print(impares)
+# for valor in valores:
+#     print(valor, end=" ")
+#     # print(f"Par: {pares}")
+#     # print(f"Impar : {impares}")
