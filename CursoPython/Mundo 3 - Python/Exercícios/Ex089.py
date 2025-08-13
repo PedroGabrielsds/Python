e3c5ec34fac0863d16efa6702f74f453f9d25edf
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-08-13 15:06:15 -0300

    Ex089 - Python Mundo 3

--- CursoPython/Mundo 3 - Python/Exercícios/Ex089.py
+++ CursoPython/Mundo 3 - Python/Exercícios/Ex089.py
@@ -3,8 +3,11 @@
 #Crie um programa que leia nome e duas notas de vários alunos e guarde de tudo em uma lista composta. No final,
 #mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno individualmente.
 
+from time import sleep
+
 boletim = []
 auxiliar = []
+alunos = []
 resposta = "S"
 
 while resposta != "N":
@@ -15,7 +18,7 @@ while resposta != "N":
     resposta = str(input(f"Deseja continuar? [S/N] ")).strip().upper()[0]
     while resposta not in "SN":
         resposta = str(input(f"Deseja continuar? [S/N] ")).strip().upper()[0]
-
+        
     auxiliar.clear()
 
 print("=-" * 40)
@@ -25,12 +28,29 @@ print(f"MÉDIA")
 
 print(f"-" * 30)
 for posicao, aluno in enumerate(boletim):
+    alunos.append(posicao)
     media = (aluno[1] + aluno[2]) / 2
     print(f"{f"{posicao}":<4}", end="")
     print(f"{f"{aluno[0]}":<20}", end="")
-    print(f"{media}")
+    print(f"{media:.2f}")
 
 print("-" * 30)
-aluno = int(input(F"Nota de qual aluno deseja ver? "))
+aluno = int(input(f"Deseja ver a nota de qual aluno(a)? (999 interrompe!) "))
 while aluno != 999:
-    
+    if(aluno not in alunos):
+        print(f"\033[1;31mAluno inexistente\033[m, tente novamente!")
+        aluno = int(input(f"Deseja ver a nota de qual aluno(a)? (999 interrompe!) "))
+    else:
+        print(f"Notas de {boletim[aluno][0]} são {boletim[aluno][1]}, {boletim[aluno][2]}")
+        print("-" * 35)
+        aluno = int(input(f"Deseja ver a nota de qual aluno(a)? (999 interrompe!) "))
+
+print(F"\033[1;31mFinalizando boletim\033[m", end="")
+contador = 0
+while(contador < 3):
+    if(contador == 3):
+        print(f"\033[1;31m.\033[m")
+    else:
+        print("\033[1;31m.\033[m", end="")
+        sleep(1)
+        contador += 1
