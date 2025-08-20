2e91db742992a50fa3bf8d6ec0761f7f80342002
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-08-20 17:59:35 -0300

    Ex094 - Python Mundo 3

--- CursoPython/Mundo 3 - Python/Exercícios/Ex094.py
+++ CursoPython/Mundo 3 - Python/Exercícios/Ex094.py
@@ -6,3 +6,45 @@
 #B) A média de idade do grupo.
 #C) Uma lista com todas as mulheres.
 #D) Uma lista com todas as pessoas com idade acima da média.
+
+grupo = []
+pessoa = {}
+acima_media = []
+mulheres = []
+resposta = 'S'
+idades = 0
+cadastros = 0
+
+while resposta != "N":
+    cadastros += 1
+    pessoa['nome'] = str(input(f"Nome: ")).strip().title()
+    pessoa['sexo'] = str(input(f"Sexo [M/F]: ")).upper().strip()[0]
+    pessoa['idade'] = int(input(f"Idade: "))
+    idades += pessoa['idade']
+    if(pessoa['sexo'] == "F"):
+        mulheres.append(pessoa['nome'])
+    grupo.append(pessoa.copy())
+    pessoa.clear()
+
+    resposta = str(input(f"Deseja continuar [S/N]: ")).upper().strip()[0]
+    while resposta not in "SN":
+        resposta = str(input(f"Deseja continuar [S/N]: ")).upper().strip()[0]
+
+media = idades / len(grupo)
+
+print("-=" * 20)
+print(f"- O grupo tem {cadastros} pessoas")
+print(f"- A média de idade é de: {media}")
+print(f"- As mulheres cadastradas foram: ", end="")
+for posicao, mulher in enumerate(mulheres):
+    if(posicao + 1 == len(mulheres)):
+        print(f"{mulher}")
+    else:
+        print(f"{mulher}, ", end="")
+
+print(f"- Lista de pessoas que estão acima da média:")
+for posicao, pessoa in enumerate(grupo):
+     if(pessoa['idade'] > media):
+         print(f"Nome: {pessoa['nome']}; sexo = {pessoa['sexo']}; idade = {pessoa['idade']}; \n")
+
+print("<< ENCERRADO >>")
