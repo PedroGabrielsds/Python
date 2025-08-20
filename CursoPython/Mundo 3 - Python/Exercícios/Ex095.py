9da71fa31216c2e096a96bc5f7405f31ab1513af
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-08-20 17:59:51 -0300

    Ex095 - Python Mundo 3

--- CursoPython/Mundo 3 - Python/Exercícios/Ex095.py
+++ CursoPython/Mundo 3 - Python/Exercícios/Ex095.py
@@ -2,3 +2,29 @@
 
 #Aprimore o DESAFIO 093 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
 
+jogadores = []
+jogador = {}
+gols = []
+resposta = "S"
+
+while resposta != "N":
+    partidas = 0
+    jogador['nome'] = str(input(f"Nome do Jogador: "))
+    partidas = int(input(f"Quantas partidas {jogador['nome']} jogou? "))
+    contador = 1
+    while contador <= partidas:
+        gols.append(int(input(f"Quantos gols na partida {contador}? ")))
+        contador += 1
+
+    jogador['gols'] = gols.copy()
+    jogadores.append(jogador.copy())
+    jogador.clear()
+    gols.clear()
+
+    resposta = str(input(f"Deseja continuar? [S/N] ")).upper().strip()[0]
+    while resposta not in "NS":
+        resposta = str(input(f"\033[1;31mOpção Incorreta\033[m! Deseja continuar? [S/N] ")).upper().strip()[0]
+    print("-" * 20)
+
+print(f"{f'Nome':>15}")
+print(f"{f"Gols":>15}")
