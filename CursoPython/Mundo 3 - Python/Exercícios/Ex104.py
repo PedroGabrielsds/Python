23fd2ea3eb4ba4a847f68db5839b1360bf9c9afa
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-08-28 16:48:33 -0300

    Exercícios - Python Mundo 3

---
+++ CursoPython/Mundo 3 - Python/Exercícios/Ex104.py
@@ -0,0 +1,6 @@
+#Aula 21 - Funções (Parte 2)
+
+#Crie um programa que tenha a função leiaint(), que vai funcionar de forma semelhante à função input() do Python.
+#Só que fazendo a validação para aceitar apenas um valor numérico.
+
+#Ex: n - leiaint("Digite um n: ")
