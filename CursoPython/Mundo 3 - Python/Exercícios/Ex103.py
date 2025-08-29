0d87aef21310ba1d2b7bbf5efac749c3cf2ebaa2
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-08-29 16:39:37 -0300

    Ex103 - Python Mundo 3

--- CursoPython/Mundo 3 - Python/Exercícios/Ex103.py
+++ CursoPython/Mundo 3 - Python/Exercícios/Ex103.py
@@ -3,7 +3,7 @@
 #Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais: o nome de um jogador e quantos gols ele marcou.
 #O programa deverá ser capaz de mostrar a ficha de jogador, mesmo que algum dado não tenha sido informado corretamente.
 
-def ficha(nome='<Desconhecido>', gols = 0):
+def ficha(nome='<Desconhecido>', gol=0):
     """
     Função responsável por mostrar o nome e gol(s) de um jogador passado pelo usuário!
     :param nome: Coleta o nome do jogador!
@@ -11,16 +11,25 @@ def ficha(nome='<Desconhecido>', gols = 0):
     :return: Sem retorno
     Função Criada Por Mack
     """
-    if(gols == 1):
-        print(f"O jogador \033[1m{nome}\033[m fez apenas \033[1m{gols}\033[m gol no campeonato!")
-    elif(gols > 1):
-        print(f"O jogador \033[1m{nome}\033[m fez \033[1m{gols}\033[m gols no campeonato!")
-    elif(gols == 0):
+    if(gol == 1):
+        print(f"O jogador \033[1m{nome}\033[m fez apenas \033[1m{gol}\033[m gol no campeonato!")
+    elif(gol > 1):
+        print(f"O jogador \033[1m{nome}\033[m fez \033[1m{gol}\033[m gols no campeonato!")
+    elif(gol == 0):
         print(f"O jogador \033[1m{nome}\033[m não fez nenhum gol no campeonato!")
 
 #Programa Principal:
 print('--' * 20)
-nome = str(input(f"Nome do jogador: "))
-gols = input(f"Número de Gols: ")
-ficha(nome, gols)
+nome = str(input(f"Nome do jogador: ")).strip()
+gols = str(input(f"Número de Gols: "))
+if(gols.isnumeric()):
+    gols = int(gols)
+else:
+    gols = 0
+
+if(nome == ''):
+    ficha(gol=gols)
+else:
+    ficha(nome, gols)
+
 help(ficha)
