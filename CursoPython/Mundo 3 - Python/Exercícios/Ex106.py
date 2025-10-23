8e289296f7bbff1c8bb6db1b5383198c2e6a7dfb
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-10-23 17:38:59 -0300

    Ex106 - Python Mundo 3

--- CursoPython/Mundo 3 - Python/Exercícios/Ex106.py
+++ CursoPython/Mundo 3 - Python/Exercícios/Ex106.py
@@ -4,27 +4,3 @@
 #Quando o usuário digitar a palavra "Fim", o programa se encerrará.
 
 #OBS: Usar cores.
-
-def escreva(frase):
-    tamanho = len(frase)
-    print("~" * tamanho)
-    print(f"{frase:^{tamanho}}")
-    print("~" * tamanho)
-
-def helplease():
-    resposta = "F"
-
-    while(resposta != "FIM"):
-        frase = escreva(f"\033[1;37;42mSISTEMA DE AJUDA PyHELP\033[m")
-        comando = str(input(f"Função ou Biblioteca >> ")).lower().strip()
-        if(comando != "FIM"):
-            frase = escreva(f"\033[1;37;44mAcessando o manual do comando '{comando}'\033[m")
-            manual = help(comando)
-            print(manual)
-            # print(f"\033[1;30;47m{manual}\033[m")
-        else:
-            frase = escreva((f"\033[1;37;41mSaindo\033[m"))
-            break
-
-#Programa Principal
-helplease()
